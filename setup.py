c7e6f8dfbca46e4bf48a48b9f5a32e2b3de0a2e1
Robert Vojta <rvojta@me.com>
2017-08-21 11:59:27 +0200

    Include *.pyui files

--- setup.py
+++ setup.py
@@ -2,12 +2,14 @@ from setuptools import setup
 
 setup(
     name='blackmamba',
-    version='0.0.4',
-    download_url = 'https://github.com/zrzka/blackmamba/archive/v0.0.4.tar.gz',
+    version='0.0.5',
+    download_url = 'https://github.com/zrzka/blackmamba/archive/v0.0.5.tar.gz',
     
     description='Pythonista on steroids',
     
     url='https://github.com/zrzka/blackmamba',
+
+    include_package_data = True,
     
     author='Robert Vojta',
     
