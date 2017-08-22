d66ff71c1190c6d942f8e33754475fecbe81eb8d
Robert Vojta <rvojta@me.com>
2017-08-22 10:36:29 +0000

    Missing file

---
+++ blackmamba/settings.py
@@ -0,0 +1,14 @@
+#!python3
+
+OPEN_QUICKLY_IGNORE_FOLDERS = {
+    '': ['.git'],
+    '.': ['.Trash', 'Examples',
+          'site-packages', 'site-packages-2', 'site-packages-3']
+}
+
+RUN_QUICKLY_IGNORE_FOLDERS = {
+    '': ['.git'],
+    '.': ['.Trash', 'Examples',
+          'site-packages', 'site-packages-2', 'site-packages-3']
+}
+
