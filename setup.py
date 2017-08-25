c56db2fd73e344e6055773f2f8cf4a21c916f471
Robert Vojta <zrzka@users.noreply.github.com>
2017-08-25 14:24:49 +0200

    Classifiers, license, keywords update

--- setup.py
+++ setup.py
@@ -5,8 +5,17 @@ setup(
     version='0.0.13',
 
     description='Pythonista on steroids',
+    
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: iOS',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Software Development'
+    ],
 
     url='https://github.com/zrzka/blackmamba',
+    license='MIT',
 
     include_package_data=True,
 
@@ -18,5 +27,5 @@ setup(
     ],
     package_dir={'': '.'},
 
-    keywords=['pythonista']
+    keywords=['pythonista ios']
 )
