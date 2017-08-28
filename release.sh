eaf84e072af72e863f52ac560723f4a16cd4f03e
Robert Vojta <rvojta@me.com>
2017-08-28 09:51:09 +0000

    Removed PyPI related files

--- release.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-VERSION=$(python setup.py --version)
-PACKAGE_PATH="dist/blackmamba-$VERSION.tar.gz"
-python setup.py sdist
-
-if [ -f "$PACKAGE_PATH" ]; then
-    twine upload $PACKAGE_PATH
-fi
