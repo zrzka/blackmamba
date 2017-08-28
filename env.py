e7f7e84096afb385d4cf2ddda887cf7bdd7fb58c
Robert Vojta <rvojta@me.com>
2017-08-28 20:57:34 +0000

    Helper

---
+++ env.py
@@ -0,0 +1,85 @@
+#!python3
+
+import shutil
+import os
+import console
+
+_ROOT_FOLDER = os.path.expanduser('~/Documents/site-packages-3/blackmamba')
+_BACKUP_FOLDER = os.path.expanduser('~/Documents/Development/bm-pip-backup')
+_GIT_FOLDER = os.path.expanduser('~/Documents/Development/bm-git')
+_GIT_MODULE_FOLDER = os.path.join(_GIT_FOLDER, 'blackmamba')
+
+
+def switch_to_git():
+    print('Switching Black Mamba to GIT version')
+
+    if not os.path.isdir(_ROOT_FOLDER):
+        print('Failed, folder does not exist: {}'.format(_ROOT_FOLDER))
+        return
+
+    if not os.path.isdir(_GIT_FOLDER):
+        print('Failed, checkout Black Mamba to: {}'.format(_GIT_FOLDER))
+        return
+
+    if os.path.isdir(_BACKUP_FOLDER):
+        print('Failed, backup folder exists: {}'.format(_BACKUP_FOLDER))
+        return
+
+    shutil.move(_ROOT_FOLDER, _BACKUP_FOLDER)
+
+    if os.path.isdir(_ROOT_FOLDER):
+        print('Failed, folder still exists: {}'.format(_ROOT_FOLDER))
+        return
+
+    os.symlink(_GIT_MODULE_FOLDER, _ROOT_FOLDER)
+
+    if not os.path.islink(_ROOT_FOLDER):
+        print('Failed, folder is not a symlink: {}'.format(_ROOT_FOLDER))
+        return
+
+    print('Black Mamba switched to GIT version')
+
+
+def switch_to_installer():
+    print('Switching Black Mamba to installer version')
+
+    if not os.path.islink(_ROOT_FOLDER):
+        print('Failed, folder is not a symlink: {}'.format(_ROOT_FOLDER))
+        return
+
+    if not os.path.isdir(_BACKUP_FOLDER):
+        print('Failed, PIP backup folder does not exist: {}'.format(_BACKUP_FOLDER))
+        return
+
+    os.remove(_ROOT_FOLDER)
+    shutil.move(_BACKUP_FOLDER, _ROOT_FOLDER)
+
+    if not os.path.isdir(_ROOT_FOLDER):
+        print('Failed, folder does not exists: {}'.format(_ROOT_FOLDER))
+        return
+
+    if os.path.isdir(_BACKUP_FOLDER):
+        print('Failed, backup folder still does exist: {}'.format(_BACKUP_FOLDER))
+        return
+
+    print('Black Mamba switched to installer version')
+
+
+def toggle():
+    try:
+        if os.path.islink(_ROOT_FOLDER):
+            console.alert('Black Mamba',
+                          'Switch to installer version?',
+                          'OK')
+            switch_to_installer()
+        else:
+            console.alert('Black Mamba',
+                          'Switch to GitHub version?',
+                          'OK')
+            switch_to_git()
+    except:
+        pass
+
+
+if __name__ == '__main__':
+    toggle()
