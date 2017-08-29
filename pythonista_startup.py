49c583f6443b9bba2ade446197b7a5580c3aae6b
Robert Vojta <rvojta@me.com>
2017-08-29 11:06:07 +0000

    Update

--- pythonista_startup.py
+++ pythonista_startup.py
@@ -5,7 +5,16 @@ import blackmamba as bm
 # ''           : any parent folder
 # '.'          : ~/Documents parent folder
 # 'Development': ~/Documents/Development parent folder
+#
+# Used for both Run Quickly & Open Quickly
 
+# Default:
+#
+# {
+#     '': ['.git'],
+#     '.': ['.Trash', 'Examples',
+#           'site-packages', 'site-packages-2', 'site-packages-3']
+# }
 bm.file_picker.ignore_folders = {
     '': ['.git'],
     '.': ['Pythonista', '.Trash', 'Examples',
@@ -14,15 +23,28 @@ bm.file_picker.ignore_folders = {
     'Development': ['bm-pip-backup']
 }
 
+# Default: 1.0
 bm.analyzer.hud_alert_delay = 1.0
+
+# Default: ['W391', 'W293']
 bm.analyzer.ignore_codes = None
+
+# Default: 79
 bm.analyzer.max_line_length = 127
+
+# Default: True
 bm.analyzer.remove_whitespaces = True
 
+# Default: 1.0
 bm.experimental.tester.hud_alert_delay = 1.0
+
+# Default: True
 bm.experimental.tester.hide_console = True
 
-bm.main()
+# Default: INFO
+bm.log.level = bm.log.NOTSET
+
+bm.main()  # <---- THIS MUST BE CALLED IN ORDER TO PROPERLY INITIALIZE BLACK MAMBA
 
 ################################################################
 # Custom keyboard shortcuts                                    #
