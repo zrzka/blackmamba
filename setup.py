b3e7c4e00a8d58f150a0457723b47ef6dee7352d
Robert Vojta <rvojta@me.com>
2017-09-02 19:46:16 +0000

    PyPI is back

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+setup(
+    name='blackmamba',
+    version='0.0.16',
+
+    description='Pythonista on steroids',
+    long_description=open('README.rst', 'rt').read(),
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: iOS',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Software Development'
+    ],
+
+    url='https://github.com/zrzka/blackmamba',
+    license='MIT',
+
+    author='Robert Vojta',
+
+    packages=[
+        'blackmamba',
+        'blackmamba.experimental'
+    ],
+    package_dir={'': '.'},
+
+    keywords=['pythonista ios']
+)
