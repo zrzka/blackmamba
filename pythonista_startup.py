29c0ffebcaef2df6bdfc6fd39356862090c78285
Robert Vojta <rvojta@me.com>
2017-09-08 13:13:44 +0000

    Sample updated

--- pythonista_startup.py
+++ pythonista_startup.py
@@ -1,79 +1,73 @@
 #!python3
 
-import blackmamba as bm
+import blackmamba
+import blackmamba.log as log
 
-# Default: True
-bm.updates.check_for_updates = True
+# Default value is INFO. Use ERROR if you'd like to make Black
+# Mamba quiet. Only errors will be printed.
+log.set_level(log.INFO)
 
-# Default: 86400 seconds (1 day)
-bm.updates.check_interval = 3600
-
-# ''           : any parent folder
-# '.'          : ~/Documents parent folder
-# 'Development': ~/Documents/Development parent folder
-#
-# Used for both Run Quickly & Open Quickly
-
-# Default:
-#
-# {
-#     '': ['.git'],
-#     '.': ['.Trash', 'Examples',
-#           'site-packages', 'site-packages-2', 'site-packages-3']
-# }
-bm.file_picker.ignore_folders = {
-    '': ['.git'],
-    '.': ['Pythonista', '.Trash', 'Examples',
-          'site-packages-2', 'site-packages', 'stash_extensions'],
-    'site-packages-3': ['blackmamba'],
-    'Development': ['bm-pip-backup']
+# Check blackmamba.config._DEFAULTS for default values
+config = {
+    'update': {
+        'enabled': True,
+        'interval': 3600
+    },
+    'file_picker': {
+        'ignore_folders': {
+            '': ['.git'],
+            '.': ['Pythonista', '.Trash', 'Examples',
+                  'site-packages-2', 'site-packages', 'stash_extensions'],
+            'site-packages-3': ['blackmamba'],
+            'Development': ['bm-pip-backup']
+        }
+    },
+    'analyzer': {
+        'hud_alert_delay': 1.0,
+        'ignore_codes': None,
+        'max_line_length': 127,
+        'remove_whitespaces': True
+    },
+    'tester': {
+        'hud_alert_delay': 1.0,
+        'hide_console': True
+    }
 }
 
-# Default: 1.0
-bm.analyzer.hud_alert_delay = 1.0
 
-# Default: ['W391', 'W293']
-bm.analyzer.ignore_codes = None
+def register_custom_shortcuts():
+    import blackmamba.ide as ide
+    import blackmamba.key_command as kc
 
-# Default: 79
-bm.analyzer.max_line_length = 127
+    # Launch `StaSh` (= custom action title) via Cmd-Shift-S
+    if ide.action_exists('StaSh'):
+        def launch_stash():
+            ide.run_action('StaSh')
 
-# Default: True
-bm.analyzer.remove_whitespaces = True
-
-# Default: 1.0
-bm.experimental.tester.hud_alert_delay = 1.0
-
-# Default: True
-bm.experimental.tester.hide_console = True
-
-# Default: INFO
-bm.log.level = bm.log.NOTSET
-
-bm.main()  # <---- THIS MUST BE CALLED IN ORDER TO PROPERLY INITIALIZE BLACK MAMBA
-
-################################################################
-# Custom keyboard shortcuts                                    #
-################################################################
-
-# Launch StaSh (= custom action title) via Cmd-Shift-S
-if bm.ide.action_exists('StaSh'):
-    def launch_stash():
-        bm.ide.run_action('StaSh')
-
-    bm.key_commands.register_key_command(
-        'S',
-        bm.uikit.UIKeyModifierCommand | bm.uikit.UIKeyModifierShift,
-        launch_stash,
-        'Launch StaSh')
+        kc.register_key_command(
+            'S',  # Shortcut key
+            kc.UIKeyModifierCommand | kc.UIKeyModifierShift,  # Shortcut modifier keys
+            launch_stash,  # Function to call
+            'Launch StaSh')  # Optional discoverability title (hold down Cmd)
 
 # Or you can use run_script instead of action to launch StaSh
-# if bm.ide.script_exists('launch_stash.py'):
+# if ide.script_exists('launch_stash.py'):
 #     def launch_stash():
-#         bm.ide.run_script('launch_stash.py')
+#         ide.run_script('launch_stash.py')
 #
-#     bm.key_commands.register_key_command(
+#     kc.register_key_command(
 #         'S',
-#         bm.uikit.UIKeyModifierCommand | bm.uikit.UIKeyModifierShift,
+#         kc.UIKeyModifierCommand | kc.UIKeyModifierShift,
 #         launch_stash,
 #         'Launch StaSh')
+
+
+def main():
+    # The only requirement is to call main(). You can omit `config=config`
+    # if you'd like to use default config.
+    blackmamba.main(config=config)
+    register_custom_shortcuts()
+
+
+if __name__ == 'pythonista_startup':
+    main()
