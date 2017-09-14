f4f771b73374e4bcb3ce812948f56028cdde906e
Robert Vojta <rvojta@me.com>
2017-09-14 12:22:24 +0200

    Hash position based on shortest whitespaces

--- blackmamba/comment.py
+++ blackmamba/comment.py
@@ -1,21 +1,14 @@
 #!python3
 
 import re
+import sys
 
 
-_COMMENT_RE = re.compile('\A(\s*)(.*?)\Z', re.DOTALL)
-
-
-def _comment_line(line):
+def _comment_line(line, hash_col_index=0):
     if not line.find('#') == -1:
         return line
 
-    match = _COMMENT_RE.search(line)
-    if match:
-        result = match.group(1) + '# ' + match.group(2)
-    else:
-        result = line
-    return result
+    return line[:hash_col_index] + '# ' + line[hash_col_index:]
 
 
 _UNCOMMENT_RE = re.compile('\A(\s*)#( ?)(.*)\Z', re.DOTALL)
@@ -31,6 +24,49 @@ def _uncomment_line(line):
     else:
         result = line
     return result
+
+
+_HASH_INDEX_RE = re.compile('\A(\s*)')
+
+
+def _hash_col_index(lines):
+    index = sys.maxsize
+
+    for line in lines:
+        match = _HASH_INDEX_RE.search(line)
+
+        if not match:
+            continue
+
+        if len(match.group(1)) < index:
+            index = len(match.group(1))
+
+    if index == sys.maxsize:
+        index = 0
+
+    return index
+
+
+def _toggle_lines(lines):
+    if not lines:
+        return lines
+
+    if lines[0].strip().startswith('#'):
+        comment = False
+        hash_col_index = None
+    else:
+        comment = True
+        hash_col_index = _hash_col_index(lines)
+
+    replacement = []
+
+    for line in lines:
+        if comment:
+            replacement.append(_comment_line(line, hash_col_index))
+        else:
+            replacement.append(_uncomment_line(line))
+
+    return replacement
 
 
 def toggle_comments():
@@ -59,15 +95,7 @@ def toggle_comments():
             del selected_lines[-1]
             selected_lines_range = (selected_lines_range[0], selected_lines_range[1] - len(last_line) - 1)
 
-    if selected_lines_text.strip().startswith('#'):
-        toggle_comment = _uncomment_line
-    else:
-        toggle_comment = _comment_line
-
-    replacement = ''
-
-    for line in selected_lines:
-        replacement += toggle_comment(line)
+    replacement = ''.join(_toggle_lines(selected_lines))
 
     if last_line_deleted:
         replacement = replacement[:-1]
--- tests/test_comment.py
+++ tests/test_comment.py
@@ -1,32 +1,36 @@
 #!python3
 
-from blackmamba.comment import _comment_line, _uncomment_line
+from blackmamba.comment import _comment_line, _uncomment_line, _toggle_lines
 
 
 _BASIC_TEST_CASES = {
     # Not indented lines
-    'a': '# a',
-    'a ': '# a ',
+    (0, 'a'): '# a',
+    (0, 'a '): '# a ',
     # Keep new line
-    'a\n': '# a\n',
+    (0, 'a\n'): '# a\n',
     # Space indented lines
-    '    a': '    # a',
-    '        a': '        # a',
+    (0, '    a'): '#     a',
+    (0, '        a'): '#         a',
     # Tab indented lines
-    '\ta': '\t# a',
-    '\t\ta': '\t\t# a',
-    '\t\ta ': '\t\t# a '
+    (0, '\ta'): '# \ta',
+    (0, '\t\ta'): '# \t\ta',
+    (0, '\t\ta '): '# \t\ta ',
+    # Hash indexes
+    (1, '\ta'): '\t# a',
+    (2, '\t\ta'): '\t\t# a',
+    (4, '    a'): '    # a'
 }
 
 
 def test_comment_line():
     for key, value in _BASIC_TEST_CASES.items():
-        assert value == _comment_line(key)
+        assert value == _comment_line(key[1], key[0])
 
 
 def test_uncomment_line():
     for key, value in _BASIC_TEST_CASES.items():
-        assert key == _uncomment_line(value)
+        assert key[1] == _uncomment_line(value)
 
 
 def test_backward_compatible_uncomment():
@@ -45,3 +49,43 @@ def test_do_not_touch_not_commented_lines():
     assert 'a b c' == _uncomment_line('a b c')
     assert ' a b c   ' == _uncomment_line(' a b c   ')
     assert '\ta b c' == _uncomment_line('\ta b c')
+
+
+_LINES_TEST_CASES = [
+    (
+        [
+            "def hallo():",
+            "    pass"
+        ],
+        [
+            "# def hallo():",
+            "#     pass"
+        ]
+    ),
+    (
+        [
+            "    def hallo():",
+            "        pass"
+        ],
+        [
+            "    # def hallo():",
+            "    #     pass"
+        ],
+    ),
+    (
+        [
+            "\t\tdef hallo():",
+            "\t\t    pass"
+        ],
+        [
+            "\t\t# def hallo():",
+            "\t\t#     pass"
+        ],
+    )
+]
+
+
+def test_toggle_comments():
+    for case in _LINES_TEST_CASES:
+        assert _toggle_lines(case[0]) == case[1]
+        assert _toggle_lines(case[1]) == case[0]
